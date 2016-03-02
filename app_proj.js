bf044c59cb1d4ad6be6ff1ec7cee4e7521fefb01
Anjelica Thang <amthang@ucsd.edu>
2016-03-02 13:43:24 -0800

    fixed buttons on profile page

--- app_proj.js
+++ app_proj.js
@@ -134,6 +134,7 @@ app_proj.get('/user_profile_diary_paris', version2.renderParis_user);
 app_proj.get('/user_profile_diary_ucsd', version2.renderUCSD_user);
 app_proj.get('/travel_diary_wine_user', version2.renderDiaryWine);
 app_proj.get('/travel_diary_brittany_user', version2.renderDiaryBrit);
+app_proj.get('/future_travels', version2.renderFutureTravels);
 
 app_proj.post('/addTrip', version2.addTripToPage);
 
