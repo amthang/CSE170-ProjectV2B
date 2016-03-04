bc2995dd3600c7b740bf5dfb5531e4b77cf086bb
Steven Duong <Steven.Duong22@gmail.com>
2016-03-03 22:14:36 -0800

    c

--- routes/search_results.js
+++ routes/search_results.js
@@ -1,6 +1,7 @@
 var results = require('../search_results.json');
 var paris_rsult = require('../paris.json');
 var sd_rsult = require('../sd.json');
+var hogwarts_rsult = require('../hogwarts.json');
 var data = require('../error_msg.json');
 
 
@@ -14,6 +15,7 @@ exports.checkSearch = function(req, res){
 		var flag = false;
 		var sd_flag = false;
 		var paris_flag = false;
+		var hogwarts_flag = false;
 
 
 		// for(var i = 0; i < results["search_results"].length; i++) {
@@ -36,6 +38,10 @@ exports.checkSearch = function(req, res){
 
 		else if (results["search_results"][1].city == city || city == "San Diego") {
 			res.render('search_results', sd_rsult);
+		}
+
+		else if (results["search_results"][2].city == city || city == "Hogwarts") {
+			res.render('search_results', hogwarts_rsult);
 		}
 
 		else
